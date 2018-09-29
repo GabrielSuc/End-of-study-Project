e8e174f858408898a8369b2ad98a9087d8882531
GabrielSuc <gabriel.suc@hotmail.com>
2018-09-29 19:02:17 +0200

    regornize multistage method, end of internship

---
+++ Smarc/smarc/test.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "get_matlab_filter.h"
+
+
+
+int main(int argc, char *argv[]){
+
+	struct getFilter* something;
+
+//	for(int i =1; i<5; i++){
+		something = get_matlab_filter(1);
+//	}
+
+return 0;}	
+	
+
+
+
+
+
